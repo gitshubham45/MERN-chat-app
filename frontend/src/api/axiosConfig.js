2b277605f648654ac115c50cee413094f448e4e3
gitshubham45 <shubham16290@gmail.com>
2024-02-14 02:25:19 +0530

    base url of axios is changed

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: "https://chat-app-41io.onrender.com/" || 'http://localhost:5000',
 });
 
 export default instance;
